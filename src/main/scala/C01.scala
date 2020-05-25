9d3fb4d80f359521e87699b265457ce854d5f2ea
Adam Hall <adamhalldesigns@gmail.com>
2020-05-25 09:06:40 +0100

    complete C01

--- src/main/scala/C01.scala
+++ src/main/scala/C01.scala
@@ -1,3 +1,46 @@
 object C01 {
+  /**
+    *  6. 
+    *  
+    *  Using BigInt, compute 2^1024.
+    */
   def computeBigInt(): BigInt = BigInt(2) pow 1024
+
+  /**
+    *  7.
+    * 
+    * What do you need to import so that you can get a random prime
+    * as `probablePrime(100, Random)`, without any qualifiers before 
+    * `probablePrime` and `Random`?
+    */
+  def genRandomPrime(): BigInt = {
+    import util.Random
+    import BigInt.probablePrime
+
+    probablePrime(100, Random)
+  }
+
+  /**
+    * 8.
+    * 
+    * One way to create random file or directory names is to produce
+    * a random `BigInt` and convert it to base 36, yielding a string 
+    * as `"qsnvbevtomcj38o06kul"`. Poke around Scaladoc to find a way 
+    * of doing this in Scala.
+    */
+  def genFileName(): String = {
+    import util.Random
+    import BigInt.probablePrime
+
+    probablePrime(100, Random).toString(36)
+  }
+
+  /**
+    *  9.
+    * 
+    * How do you get the first character of a string in Scala? The last character?
+    */
+  def firstAndLast(s: String): (Char, Char) = {
+    (s.head, s.last)
+  }
 }
--- src/test/scala/C01Spec.scala
+++ src/test/scala/C01Spec.scala
@@ -3,10 +3,24 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class C01Spec extends FlatSpec with Matchers {
   "computeBigInt" should "compute 2 pow 1024" in {
-    //when & then
     computeBigInt() shouldBe BigInt("1797693134862315907729305190789024733617976978942306572734" +
       "300811577326758055009631327084773224075360211201138798713933576587897688144166224928474" +
       "3063947412437776789342486548527630221960124609411945308295208500576883815068234246288147" +
       "3913110540827237163350510684586298239947245938479716304835356329624224137216")
+  }
+
+  "genRandomPrime" should "compute random numbers" in {
+    genRandomPrime() should not be genRandomPrime()
+  }
+
+  "getFileName" should "generate random file names" in {
+    val res = genFileName()
+
+    res.length() shouldBe 20
+    res.matches("[a-z0-9]*") shouldBe true
+  }
+
+  "firstAndLast" should "get the first and last letter in string" in {
+    firstAndLast("hello") shouldBe ('h', 'o')
   }
 }
