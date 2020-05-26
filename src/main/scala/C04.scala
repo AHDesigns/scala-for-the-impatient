d0f70317771a2ac7c244541ae47de69fa2654a7f
Adam Hall <adamhalldesigns@gmail.com>
2020-05-26 20:56:07 +0100

    add C04 wip

---
+++ src/main/scala/C04.scala
@@ -0,0 +1,13 @@
+object C04 {
+
+  /**
+    * 1.
+    * 
+    * Set up a map of prices for a number of gizmos that you covet. Then produce
+    * a second map with the same keys and the prices at a 10 percent discount.
+    */
+  val gizmos = Map(
+    "iPhone" -> 600,
+    "car" -> 30000,
+  )
+}
---
+++ src/test/scala/C04Spec.scala
@@ -0,0 +1,7 @@
+import C04._
+import org.scalatest.{FlatSpec, Matchers}
+
+class C04Spec extends FlatSpec with Matchers {
+  "randomArr" should "create array of n random ints between 0 and n -1" in {
+  }
+}
